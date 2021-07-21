d680d83c9d02f5471e938418888b01681da32c9c
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-07-21 22:54:44 +0530

    Created the content of the Profile fragment

--- app/src/main/java/com/example/myapp/ProfileFragment.kt
+++ app/src/main/java/com/example/myapp/ProfileFragment.kt
@@ -1,6 +1,20 @@
 package com.example.myapp
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.profile_fragment.*
 
 class ProfileFragment: Fragment(R.layout.profile_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnCodechef.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://www.codechef.com/users/simba_98")
+            startActivity(openURL)
+        }
+    }
 }
