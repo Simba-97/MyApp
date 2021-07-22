97fa9d09621970e682ebf411fb37aa6839869d79
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-07-22 14:23:44 +0530

    Added Location and Email Intent

--- app/src/main/java/com/example/myapp/ContactFragment.kt
+++ app/src/main/java/com/example/myapp/ContactFragment.kt
@@ -12,10 +12,21 @@ class ContactFragment: Fragment(R.layout.contact_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnCall.setOnClickListener{
-            val intent = Intent(android.content.Intent.ACTION_DIAL)
-            intent.data = Uri.parse("tel:8167576728")
-            startActivity(intent)
+        btnCall.setOnClickListener {
+            val callIntent = Intent(android.content.Intent.ACTION_DIAL)
+            callIntent.data = Uri.parse("tel:8167576728")
+            startActivity(callIntent)
+        }
+
+        btnMail.setOnClickListener {
+            val mailIntent = Intent(android.content.Intent.ACTION_SENDTO, Uri.fromParts("mailto", "singhhritik978@gmail.com", null))
+            startActivity(mailIntent)
+        }
+
+        btnLocation.setOnClickListener {
+            val location = "https://www.google.com/maps/place/Newtown,+Kolkata,+West+Bengal/@22.5862314,88.4577813,13z/data=!3m1!4b1!4m5!3m4!1s0x3a0275350398a5b9:0x75e165b244323425!8m2!3d22.5753931!4d88.4797903"
+            val locationIntent = Intent(android.content.Intent.ACTION_VIEW, Uri.parse(location))
+            startActivity(locationIntent)
         }
     }
 }
